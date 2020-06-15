0a48d0ff7224a4ff92db94ca12c7df68f0770944
Dung <dung.nguyen.tien.0810@gmail.com>
2020-06-15 20:31:01 +0700

    Demo basic

--- Demo/HelloWorld/src/app/demo.component.ts
+++ Demo/HelloWorld/src/app/demo.component.ts
@@ -13,8 +13,19 @@ export class DemoComponent implements OnInit {
     status : boolean;
     username : string;
     phone : any;
-    
+    photo : string;
+    photos : string[];
     ngOnInit() {
-
+        this.age = 100;
+        this.price = 10.4;
+        this.quantity = 10;
+        this.status = true;
+        this.username = "asd";
+        this.phone = "12320";
+        this.photo = "FB_IMG_1533218193931.jpg";
+        this.photos = [
+            "FB_IMG_1529509183911.jpg",
+            "FB_IMG_1530102590413.jpg",
+        ];
     }
 }
